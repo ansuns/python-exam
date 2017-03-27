e1d05bafb27ed7be261cecbee7bd27cf5da7a7bb
Yangs <251024883@qq.com>
2017-03-27 11:38:35 +0800

    b'str'可以表示字节

--- builtin_module/base64_module.py
+++ builtin_module/base64_module.py
@@ -8,6 +8,7 @@ s = base64.b64encode('aaa')
 print(s)
 
 #由于标准的Base64编码后可能出现字符+和/，在URL中就不能直接作为参数，所以又有一种"url safe"的base64编码，其实就是把字符+和/分别变成-和_：
+#b'str'可以表示字节
 s = base64.urlsafe_b64encode(b'i\xb7\x1d\xfb\xef\xff')
 print(s)
 
