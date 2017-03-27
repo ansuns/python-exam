eeb6b625e92f189ca7b595a384d3882c55edc5cb
Yangs <251024883@qq.com>
2017-03-27 17:03:18 +0800

    复习1:hellor world!

--- basic/helloworld.py
+++ basic/helloworld.py
@@ -2,3 +2,19 @@
 # -*- coding: utf-8 -*-
 __author__ = 'RoLiHop'
 print('Hello World!')
+#函数也可以接受多个字符串，用逗号“,”隔开，就可以连成一串输出：
+print('a', 'b', 'c', 'd') # a b c d
+
+name = input('please input your name: \n')
+
+print('Hello %s' % name)
+
+a = int(input('input a number:\n'))
+if a > 0:
+    a = a
+else:
+    a = -a
+print(a)
+
+
+
