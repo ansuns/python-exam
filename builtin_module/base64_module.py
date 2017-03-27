8902a8ea7a7d8c7dc9205bfc57c7e15bdfe360c0
Yangs <251024883@qq.com>
2017-03-27 11:53:58 +0800

    base64

--- builtin_module/base64_module.py
+++ builtin_module/base64_module.py
@@ -4,13 +4,13 @@ __author__ = 'RoLiHop'
 
 import base64
 
-s = base64.b64encode('aaa')
+s = base64.b64encode(b'a')
 print(s)
 
 #由于标准的Base64编码后可能出现字符+和/，在URL中就不能直接作为参数，所以又有一种"url safe"的base64编码，其实就是把字符+和/分别变成-和_：
 #b'str'可以表示字节
-s = base64.urlsafe_b64encode(b'i\xb7\x1d\xfb\xef\xff')
-print(s)
+sa = base64.urlsafe_b64encode(b'i\xb7\x1d\xfb\xef\xff')
+print(sa)
 
 '''
 Base64是一种用64个字符来表示任意二进制数据的方法。
