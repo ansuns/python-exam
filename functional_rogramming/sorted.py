e3fc0ccb570317331c6efa4664e0becf975e877b
Yangs <251024883@qq.com>
2017-04-01 16:53:03 +0800

    复习：sorted排序

--- functional_rogramming/sorted.py
+++ functional_rogramming/sorted.py
@@ -5,13 +5,23 @@ __author__ = 'RoLiHop'
 #排序算法
 
 L = [-29, 1, 0, 21, 444, -1231, 111, 200, 15]
-
+print(L)
 res = sorted(L)
 print(res)
 
 #key函数来实现自定义的排序
 res = sorted(L, key = abs)
 print(res)
+
+
+
+s = ['Bob', 'Ukick', 'Mary', 'bill', 'Zoo', 'Sruiy']
+print(s)
+print(sorted(s))
+print(sorted(s, key=str.lower))
+
+#逆向排序 reverse = True
+print(sorted(s, key=str.lower, reverse = True))
 '''
 排序也是在程序中经常用到的算法。无论使用冒泡排序还是快速排序，排序的核心是比较两个元素的大小。如果是数字，我们可以直接比较，但如果是字符串或者两个dict呢？直接比较数学上的大小是没有意义的，因此，比较的过程必须通过函数抽象出来。
 
